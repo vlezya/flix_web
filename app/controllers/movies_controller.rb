190a0ddc315b98bd2456801550021c50d43cfd27
vlezya <vlezyashop@gmail.com>
2023-01-17 23:20:21 +0200

    Add flash messages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,15 +11,16 @@ class MoviesController < ApplicationController
   
   def update
     if @movie.update(movie_params)
-      redirect_to @movie
+      redirect_to @movie, notice: 'Movie was successfully updated'
     else
       render :edit, status: :unprocessable_entity
+    end
   end
   
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
-      redirect_to @movie
+      redirect_to @movie, notice: 'Movie was successfully created'
     else
       render :new, status: :unprocessable_entity
     end
@@ -31,7 +32,8 @@ class MoviesController < ApplicationController
   
   def destroy
     @movie.destroy
-    redirect_to movies_path, status: :see_other
+    redirect_to movies_path, status: :see_other,
+      alert: "Movie has been successfully destroyed"
   end
   
   private
