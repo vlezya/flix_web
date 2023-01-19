ac7bf0f03c351d8227109a7a6c9db00e6c99a777
vlezya <vlezyashop@gmail.com>
2023-01-19 14:24:34 +0200

    Add movies avrage stars after reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,21 +15,16 @@ class Movie < ApplicationRecord
   def self.release
     where('released_on < ?', Time.now).order('released_on desc') # or .order(released_on: :desc)
   end
-  
-  def self.hits
-    where("total_gross >= 300000000").order(total_gross: :desc)
-  end
-  
-  def self.flops
-    where("total_gross < 22500000").order(total_gross: :asc)
-  end
-  
-  def self.recently_added
-    order("created_at desc").limit(3)
-  end
-  
+    
   def flop?
     total_gross.blank? || total_gross < 255_000_000
   end
   
+  def average_stars
+    reviews.average(:stars) || 0.0
+  end
+  
+  def average_stars_as_percent
+    (self.average_stars / 5.0) * 100
+  end
 end
