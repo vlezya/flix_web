5a11de38dfe06fe003455089a061ffc977effcac
vlezya <vlezyashop@gmail.com>
2023-01-19 09:57:19 +0200

    Add new form and create action for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,31 @@
 class ReviewsController < ApplicationController
+  before_action :set_review
+  
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
+  end
+  
+  def new
+    @review = @movie.reviews.new
+  end
+  
+  def create
+    @review = @movie.reviews.new(params_review)
+    
+    if @review.save
+      redirect_to movie_reviews_path, notice: 'Thanks for you review'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
+  def set_review
+    @movie = Movie.find(params[:movie_id])
+  end
+  
+  def params_review
+    params.require(:review).permit(:name, :comment, :stars)
   end
 end
